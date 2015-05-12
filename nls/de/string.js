9caced4a0643b62a04bdd333459c162c8980ec12
XavBX <XavBX@users.noreply.github.com>
2015-05-12 10:07:15 +0200

    German translation
    Add German translation

---
+++ nls/de/string.js
@@ -0,0 +1,35 @@
+/*
+* Copyright (c) 2014 Denisov21. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+define({
+"RIGHT_CLICK_MENU_CUT_NAME" : "Ausschneiden",
+"RIGHT_CLICK_MENU_COPY_NAME" : "Kopieren",
+"RIGHT_CLICK_MENU_PASTE_NAME" : "Einfügen",
+"RIGHT_CLICK_MENU_UPPERCASE_NAME" : "Groß",
+"RIGHT_CLICK_MENU_LOWERCASE_NAME" : "winzig",
+"RIGHT_CLICK_MENU_CAMELCASE_NAME" : "Camel Case",
+"RIGHT_CLICK_MENU_BLOCKCOMMENT_NAME" : "Block (aus-)kommentieren",
+"RIGHT_CLICK_MENU_LINECOMMENT_NAME" : "Zeile (aus-)kommentieren",
+"RIGHT_CLICK_MENU_SAVEALL_NAME" : "Alles speichern"
+});
