f5139347e4f5860e4bb8827c0d0cf2fdfd81df37
XavBX <XavBX@users.noreply.github.com>
2015-05-17 18:27:51 +0200

    A lot of change
    New features :
    - Add sub menu for "Convertion" action;
    - Add Encoding/Decoding HTML Entity;
    - Add Encoding/Decoding URI;

--- main.js
+++ main.js
@@ -13,8 +13,11 @@ define(function (require, exports, module) {
         Commands = brackets.getModule("command/Commands"),
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
         NodeDomain = brackets.getModule("utils/NodeDomain"),
-        ProjectManager = brackets.getModule("project/ProjectManager");
-
+        ProjectManager = brackets.getModule("project/ProjectManager"),
+		StringUtils = brackets.getModule("utils/StringUtils"),
+		ViewUtils = brackets.getModule("utils/ViewUtils"),
+		FileUtils = brackets.getModule("file/FileUtils"),
+		he = require("vendor/he");
 
     /*
         Some constants used by Additional right click menu       
@@ -36,11 +39,24 @@ define(function (require, exports, module) {
         RIGHT_CLICK_MENU_BLOCKCOMMENT_NAME   = "Toggle Block Comment",
         RIGHT_CLICK_MENU_BLOCKCOMMENT_COMMAND_ID  = "rightclickmenu.blockComment",
         RIGHT_CLICK_MENU_LINECOMMENT_NAME   = "Toggle Line Comment",
-        RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID  = "rightclickmenu.lineComment";/*,
+        RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID  = "rightclickmenu.lineComment",/*,
         RIGHT_CLICK_MENU_COPYFILE_NAME   = "Copy",
         RIGHT_CLICK_MENU_COPYFILE_COMMAND_ID  = "rightclickmenu.copyFile",
         RIGHT_CLICK_MENU_PASTEFILE_NAME   = "Paste",
         RIGHT_CLICK_MENU_PASTEFILE_COMMAND_ID  = "rightclickmenu.pasteFile";*/
+		RIGHT_CLICK_MENU_CONVERTTO_NAME = "Convert to",
+		RIGHT_CLICK_MENU_CONVERTTO_COMMAND_ID = "rightclickmenu.convertTo",
+		RIGHT_CLICK_MENU_ENCODE_HTMLENTITY_NAME = "Encode HTML entity",
+		RIGHT_CLICK_MENU_ENCODE_HTMLENTITY_COMMAND_ID = "rightclickmenu.encodeHTMLentity",
+		RIGHT_CLICK_MENU_ENCODE_NAMEDHTMLENTITY_NAME = "Encode Named HTML entity",
+		RIGHT_CLICK_MENU_ENCODE_NAMEDHTMLENTITY_COMMAND_ID = "rightclickmenu.encodeNamedHTMLentity",
+		RIGHT_CLICK_MENU_DECODE_HTMLENTITY_NAME = "Decode HTML entity",
+		RIGHT_CLICK_MENU_DECODE_HTMLENTITY_COMMAND_ID = "rightclickmenu.decodeHTMLentity",
+		RIGHT_CLICK_MENU_ENCODE_URI_NAME = "Encode URI",
+		RIGHT_CLICK_MENU_ENCODE_URI_COMMAND_ID = "rightclickmenu.encodeURI",
+		RIGHT_CLICK_MENU_DECODE_URI_NAME = "Decode URI",
+		RIGHT_CLICK_MENU_DECODE_URI_COMMAND_ID = "rightclickmenu.decodeURI";
+	
     var Strings             = require("strings");
     
     var RIGHT_CLICK_MENU_SAVEALL_NAME   = "Save All",
@@ -60,6 +76,10 @@ define(function (require, exports, module) {
                 break;
             case 3:
                 //alert('Right Mouse button pressed.');
+				if ($("#editor-context-menu-rightclickmenu\\.convertTo span.menu-shortcut").length === 0) {
+					$("#editor-context-menu-rightclickmenu\\.convertTo").append('<span class="menu-shortcut">&rtrif;</span>');
+				}
+				
                 if(el.find('.CodeMirror-code').length > 0){
                     var editor = EditorManager.getCurrentFullEditor(),
                         selectedText = editor.getSelectedText(),
@@ -79,7 +99,12 @@ define(function (require, exports, module) {
                         if($('#editor-context-menu-rightclickmenu\\.cut\\.disabled').length===0){
                             $('<li>'+li.html().replace('editor-context-menu-rightclickmenu.cut', 'editor-context-menu-rightclickmenu.cut.disabled')+'</li>').insertBefore(li).find('a').css('color', '#494949');
                         }
-
+						li = $('#editor-context-menu-rightclickmenu\\.convertTo').parent();
+						li.hide();
+                        if($('#editor-context-menu-rightclickmenu\\.convertTo\\.disabled').length===0){
+                            $('<li>'+li.html().replace('editor-context-menu-rightclickmenu.convertTo', 'editor-context-menu-rightclickmenu.convertTo.disabled')+'</li>').insertBefore(li).find('a').css('color', '#494949');
+                        }
+						/*
                         li = $('#editor-context-menu-rightclickmenu\\.uppercase').parent();
                         li.hide();
                         if($('#editor-context-menu-rightclickmenu\\.uppercase\\.disabled').length===0){
@@ -97,7 +122,7 @@ define(function (require, exports, module) {
                         if($('#editor-context-menu-rightclickmenu\\.camelcase\\.disabled').length===0){
                             $('<li>'+li.html().replace('editor-context-menu-rightclickmenu.camelcase', 'editor-context-menu-rightclickmenu.camelcase.disabled')+'</li>').insertBefore(li).find('a').css('color', '#494949');
                         }
-                    
+                    	*/
                     }
                     else{
                         setCursorPos = false;
@@ -108,11 +133,14 @@ define(function (require, exports, module) {
                         li = $('#editor-context-menu-rightclickmenu\\.cut').parent();
                         li.show();
                         $('#editor-context-menu-rightclickmenu\\.cut\\.disabled').remove();
-
+						
+                        li = $('#editor-context-menu-rightclickmenu\\.convertTo').parent();
+                        li.show();
+                        $('#editor-context-menu-rightclickmenu\\.convertTo\\.disabled').remove();
+						/*
                         li = $('#editor-context-menu-rightclickmenu\\.lowercase').parent();
                         li.show();
                         $('#editor-context-menu-rightclickmenu\\.lowercase\\.disabled').remove();
-
                         
                         li = $('#editor-context-menu-rightclickmenu\\.uppercase').parent();
                         li.show();
@@ -121,9 +149,21 @@ define(function (require, exports, module) {
                         li = $('#editor-context-menu-rightclickmenu\\.camelcase').parent();
                         li.show();
                         $('#editor-context-menu-rightclickmenu\\.camelcase\\.disabled').remove();
-                        
+                        */
                     }
                 }
+				
+				if ($('#editor-context-menu-rightclickmenu\\.convertTo\\.event').length === 0) {
+					$('#editor-context-menu *[id^="editor-context-menu"]').mouseover(function(event) {
+						if (this.id === "editor-context-menu-rightclickmenu.convertTo") {
+							Menus.getContextMenu("convertto-context-menu").openSubMenu(event);
+						} else {
+							Menus.getContextMenu("convertto-context-menu").close(event);
+						}
+					});
+					
+					$('#editor-context-menu-rightclickmenu\\.convertTo').toggleClass('event');
+				}
                 break;
         }
     });
@@ -194,6 +234,52 @@ define(function (require, exports, module) {
         
         editor.setSelection(pos.start, pos.end);
     }
+	
+	function decodeHTMLEntity() {
+		var editor = EditorManager.getCurrentFullEditor(),
+			selectedText = he.decode(editor.getSelectedText()),
+			pos = editor.getSelection(),
+			currentDoc = DocumentManager.getCurrentDocument();
+		currentDoc.replaceRange(selectedText, pos.start, pos.end);
+	}
+	
+	function encodeHTMLEntity() {
+		var editor = EditorManager.getCurrentFullEditor(),
+			selectedText = he.encode(editor.getSelectedText(), {
+				'encodeEverything': false,
+				'useNamedReferences': false
+			}),
+			pos = editor.getSelection(),
+			currentDoc = DocumentManager.getCurrentDocument();
+		currentDoc.replaceRange(selectedText, pos.start, pos.end);
+	}
+	
+	function encodeNamedHTMLEntity() {
+		var editor = EditorManager.getCurrentFullEditor(),
+			selectedText = he.encode(editor.getSelectedText(), {
+				'encodeEverything': false,
+				'useNamedReferences': true
+			}),
+			pos = editor.getSelection(),
+			currentDoc = DocumentManager.getCurrentDocument();
+		currentDoc.replaceRange(selectedText, pos.start, pos.end);
+	}
+	
+	function encodeTextToURI() {
+		var editor = EditorManager.getCurrentFullEditor(),
+			selectedText = encodeURIComponent(editor.getSelectedText()),
+			pos = editor.getSelection(),
+			currentDoc = DocumentManager.getCurrentDocument();
+		currentDoc.replaceRange(selectedText, pos.start, pos.end);
+	}
+	
+	function decodeURIToText() {
+		var editor = EditorManager.getCurrentFullEditor(),
+			selectedText = decodeURIComponent(editor.getSelectedText()),
+			pos = editor.getSelection(),
+			currentDoc = DocumentManager.getCurrentDocument();
+		currentDoc.replaceRange(selectedText, pos.start, pos.end);
+	}
 
     function blockComment(){
         CommandManager.execute(Commands.EDIT_BLOCK_COMMENT);
@@ -233,6 +319,10 @@ define(function (require, exports, module) {
 
     }
 
+	function doNothing() {
+		return false;
+	}
+	
     /*
         Register command for menu action
     */
@@ -246,9 +336,15 @@ define(function (require, exports, module) {
     CommandManager.register(Strings.RIGHT_CLICK_MENU_BLOCKCOMMENT_NAME, RIGHT_CLICK_MENU_BLOCKCOMMENT_COMMAND_ID, blockComment);
     CommandManager.register(Strings.RIGHT_CLICK_MENU_LINECOMMENT_NAME, RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID, lineComment);
    
-     CommandManager.register(Strings.RIGHT_CLICK_MENU_SAVEALL_NAME, RIGHT_CLICK_MENU_SAVEALL_COMMAND_ID, saveAll);
+    CommandManager.register(Strings.RIGHT_CLICK_MENU_SAVEALL_NAME, RIGHT_CLICK_MENU_SAVEALL_COMMAND_ID, saveAll);
     //CommandManager.register(RIGHT_CLICK_MENU_COPYFILE_NAME, RIGHT_CLICK_MENU_COPYFILE_COMMAND_ID, copyFile);
     //CommandManager.register(RIGHT_CLICK_MENU_PASTEFILE_NAME, RIGHT_CLICK_MENU_PASTEFILE_COMMAND_ID, pasteFile);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_CONVERTTO_NAME, RIGHT_CLICK_MENU_CONVERTTO_COMMAND_ID, doNothing);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_ENCODE_HTMLENTITY_NAME, RIGHT_CLICK_MENU_ENCODE_HTMLENTITY_COMMAND_ID, encodeHTMLEntity);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_ENCODE_NAMEDHTMLENTITY_NAME, RIGHT_CLICK_MENU_ENCODE_NAMEDHTMLENTITY_COMMAND_ID, encodeNamedHTMLEntity);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_DECODE_HTMLENTITY_NAME, RIGHT_CLICK_MENU_DECODE_HTMLENTITY_COMMAND_ID, decodeHTMLEntity);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_ENCODE_URI_NAME, RIGHT_CLICK_MENU_ENCODE_URI_COMMAND_ID, encodeTextToURI);
+	CommandManager.register(Strings.RIGHT_CLICK_MENU_DECODE_URI_NAME, RIGHT_CLICK_MENU_DECODE_URI_COMMAND_ID, decodeURIToText);
 
 
     /*
@@ -260,9 +356,12 @@ define(function (require, exports, module) {
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_PASTE_COMMAND_ID);
     //Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_SELECTALL_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
+	/*
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_CAMELCASE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID);
+	*/
+	Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_CONVERTTO_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_BLOCKCOMMENT_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID);
@@ -278,5 +377,58 @@ define(function (require, exports, module) {
 
     //KeyBindingManager.addBinding(RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID, 'Ctrl-U');
     //KeyBindingManager.addBinding(RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID, 'Ctrl-L');
+	
+	var convertTo_cmenu = Menus.registerContextMenu("convertto-context-menu");
+	
+    convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID);
+    convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID);
+    convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_CAMELCASE_COMMAND_ID);
+    convertTo_cmenu.addMenuDivider();
+	convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_ENCODE_HTMLENTITY_COMMAND_ID);
+	convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_ENCODE_NAMEDHTMLENTITY_COMMAND_ID);
+	convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_DECODE_HTMLENTITY_COMMAND_ID);
+    convertTo_cmenu.addMenuDivider();
+	convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_ENCODE_URI_COMMAND_ID);
+	convertTo_cmenu.addMenuItem(RIGHT_CLICK_MENU_DECODE_URI_COMMAND_ID);
+	
+    convertTo_cmenu.openSubMenu = function (mouseOrLocation) {
+		
+		if (mouseOrLocation.target.nodeName === "A") {
+			var $window = $(window),
+				escapedId = StringUtils.jQueryIdEscape(this.id),
+				$menuAnchor = $("#" + escapedId),
+				$menuWindow = $("#" + escapedId + " > ul"),
+				posTop  = parseInt($(".dropdown.context-menu.open").css("top")) + mouseOrLocation.target.offsetTop,
+				posLeft = parseInt($(".dropdown.context-menu.open").css("left")) + mouseOrLocation.target.offsetWidth;
+
+			// only show context menu if it has menu items
+			if ($menuWindow.children().length <= 0) {
+				return;
+			}
+
+			// adjust positioning so menu is not clipped off bottom or right
+			var elementRect = {
+					top:    posTop,
+					left:   posLeft,
+					height: $menuWindow.height() + 25,
+					width:  $menuWindow.width()
+				},
+				clip = ViewUtils.getElementClipSize($window, elementRect);
+
+			if (clip.bottom > 0) {
+				posTop = Math.max(0, posTop - clip.bottom);
+			}
+			posLeft -= 5;
+
+
+			if (clip.right > 0) {
+				posLeft = Math.max(0, parseInt($(".dropdown.context-menu.open").css("left")) - elementRect.width + 5);
+			}
+
+			// open the context menu at final location
+			$menuAnchor.addClass("open")
+					   .css({"left": posLeft, "top": posTop});
+		}
+    };
 
 });
